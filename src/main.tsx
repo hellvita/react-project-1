9abdb41f6962919e38f41a26b28bf381142c1682
hellvita <seredaolga03@gmail.com>
2025-11-19 14:03:45 +0200

    change main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+// main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <div>
+    <h1>Welcome to React</h1>
+    <p>
+      This is JSX — it looks like HTML, but it's not quite the same. It has its
+      own rules!
+    </p>
+  </div>
+);
