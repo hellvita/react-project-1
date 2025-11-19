70ee822541e73e78ecee81c650bc618d882569f8
hellvita <seredaolga03@gmail.com>
2025-11-19 14:34:42 +0200

    complete the Product component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,9 +6,21 @@ export default function App() {
     <>
       <h1>Products</h1>
 
-      <Product name="Tacos With Lime" />
-      <Product name="Fries and Burger" />
-      <Product name="Delicious Pizza" />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
+      <Product
+        name="Delicious Pizza"
+        imgUrl="https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg"
+        price={8.98}
+      />
     </>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,15 @@
 interface ProductProps {
   name: string;
+  imgUrl: string;
+  price: number;
 }
 
 export default function Product(props: ProductProps) {
   return (
     <div>
       <h2>{props.name}</h2>
-      <p>Price: 999 credits</p>
+      <img src={props.imgUrl} alt={props.name} width={640} />
+      <p>Price: {props.price} credits</p>
     </div>
   );
 }
