56d452ddd61b65763da32e817d2e24db4fcc46c2
hellvita <seredaolga03@gmail.com>
2025-11-19 14:11:44 +0200

    add img

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import "./index.css";
 // main.tsx
+
 import { createRoot } from "react-dom/client";
 
 const techName = "React";
+const imgUrl =
+  "https://blog.logrocket.com/wp-content/uploads/2025/02/advanced-react-state-management-using-url-parameters.png";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <>
@@ -11,5 +14,6 @@ createRoot(document.getElementById("root") as HTMLElement).render(
       This is JSX — it looks like HTML, but it's not quite the same. It has its
       own rules!
     </p>
+    <img src={imgUrl} alt="React logo" width="895" />
   </>
 );
