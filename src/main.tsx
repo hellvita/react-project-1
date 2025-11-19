0b608a3ed72f1a430857c9a7e8f0cac45100ad38
hellvita <seredaolga03@gmail.com>
2025-11-19 14:07:45 +0200

    use variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import "./index.css";
 // main.tsx
-
 import { createRoot } from "react-dom/client";
 
+const techName = "React";
+
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
+  <>
+    <h1>Welcome to {techName}</h1>
     <p>
       This is JSX — it looks like HTML, but it's not quite the same. It has its
       own rules!
     </p>
-  </div>
+  </>
 );
