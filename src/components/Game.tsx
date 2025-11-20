0b5287bf9af4782da7ab2135a6cc0c256757d8ec
hellvita <seredaolga03@gmail.com>
2025-11-20 14:07:33 +0200

    add the count props

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,18 +1,5 @@
 import GridContainer from "../components/GridContainer";
-import GridItem from "../components/GridItem";
 
 export default function Game() {
-  return (
-    <GridContainer>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-      <GridItem></GridItem>
-    </GridContainer>
-  );
+  return <GridContainer count={25} />;
 }
--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import css from "../styles/GridContainer.module.css";
+import GridItem from "./GridItem";
 
 interface GridContainerProps {
-  children: React.ReactNode;
+  count: number;
 }
 
-export default function GridContainer({ children }: GridContainerProps) {
-  const items = React.Children.toArray(children).filter(
-    (item) => item !== null
-  );
-  const itemCount = items.length;
-
-  const gridSize = Math.ceil(Math.sqrt(itemCount));
+export default function GridContainer({ count }: GridContainerProps) {
+  const gridSize = Math.ceil(Math.sqrt(count));
+  const itemsToRender = Array(count).fill(0);
   const gridStyle: React.CSSProperties = {
     gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
   };
   return (
     <div className={css["grid-container"]} style={gridStyle}>
-      {children}
+      {itemsToRender.map((_, index) => (
+        <GridItem key={index}></GridItem>
+      ))}
     </div>
   );
 }
