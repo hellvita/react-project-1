c7de7ccc6f80a498198e44b9295763d97d4579f0
hellvita <seredaolga03@gmail.com>
2025-11-20 14:44:06 +0200

    add ability to change color by moving cursor (on click)

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "../styles/GridContainer.module.css";
 import GridItem from "./GridItem";
 
@@ -8,14 +8,52 @@ interface GridContainerProps {
 
 export default function GridContainer({ count }: GridContainerProps) {
   const gridSize = Math.ceil(Math.sqrt(count));
+
+  const [gridState, setGridState] = useState<boolean[]>(
+    Array(count).fill(false)
+  );
+  const [isDrawing, setIsDrawing] = useState(false);
+
   const itemsToRender = Array(count).fill(0);
   const gridStyle: React.CSSProperties = {
     gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
   };
+
+  const handleMouseDown = (index: number) => {
+    setIsDrawing(true);
+    setGridState((prev) => {
+      const newGrid = [...prev];
+      newGrid[index] = !newGrid[index];
+      return newGrid;
+    });
+  };
+
+  const handleMouseUp = () => setIsDrawing(false);
+
+  const handleMouseEnter = (index: number) => {
+    if (!isDrawing) return;
+    setGridState((prev) => {
+      const newGrid = [...prev];
+      newGrid[index] = !newGrid[index];
+      return newGrid;
+    });
+  };
+
   return (
-    <div className={css["grid-container"]} style={gridStyle}>
+    <div
+      className={css["grid-container"]}
+      style={gridStyle}
+      onMouseLeave={handleMouseUp}
+      onMouseUp={handleMouseUp}
+    >
       {itemsToRender.map((_, index) => (
-        <GridItem key={index}></GridItem>
+        <GridItem
+          key={index}
+          index={index}
+          isActive={gridState[index]}
+          onMouseDown={handleMouseDown}
+          onMouseEnter={handleMouseEnter}
+        ></GridItem>
       ))}
     </div>
   );
--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import clsx from "clsx";
 import css from "../styles/GridItem.module.css";
 
 interface GridItemProps {
   children?: React.ReactNode;
+  index: number;
+  isActive: boolean;
+  onMouseDown: (index: number) => void;
+  onMouseEnter: (index: number) => void;
 }
 
-export default function GridItem({ children }: GridItemProps) {
-  const [isColored, setIsColored] = useState(false);
-  const onClick = () => setIsColored((prev) => !prev);
-
+export default function GridItem({
+  children,
+  index,
+  isActive,
+  onMouseDown,
+  onMouseEnter,
+}: GridItemProps) {
   return (
     <div
       className={clsx(
         css["grid-item"],
-        isColored ? css["grid-item--active"] : ""
+        isActive ? css["grid-item--active"] : ""
       )}
-      onClick={onClick}
+      onMouseDown={() => onMouseDown(index)}
+      onMouseEnter={() => onMouseEnter(index)}
     >
       {children}
     </div>
